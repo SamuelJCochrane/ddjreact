dacaee41f60bf150ddec799ac4807875d1cabfd8
SamuelJCochrane <32472108+SamuelJCochrane@users.noreply.github.com>
2018-05-22 20:53:02 +0100

    message

--- src/Calendar.js
+++ src/Calendar.js
@@ -13,11 +13,20 @@ export class Calendar extends Component {
         this.daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
         this.toggleCalendar = this.toggleCalendar.bind(this);
-        this.testFunc = this.testFunc.bind(this);
+        this.calendarRight = this.calendarRight.bind(this);
+        this.calendarLeft = this.calendarLeft.bind(this);
     }
 
-    testFunc() {
-        console.log(this.state.selectedDate.getDay());
+    calendarRight() {
+        const [ currentMonth, currentYear ] = [ this.state.selectedDate.getMonth(), this.state.selectedDate.getFullYear() ]
+        const newMonth = currentMonth + 1;
+        this.setState({ selectedDate: new Date(currentYear, newMonth)})
+    }
+
+    calendarLeft() {
+        const [ currentMonth, currentYear ] = [ this.state.selectedDate.getMonth(), this.state.selectedDate.getFullYear() ];
+        const newMonth = currentMonth - 1;
+        this.setState({ selectedDate: new Date(currentYear, newMonth)});
     }
 
     toggleCalendar() {
@@ -25,12 +34,44 @@ export class Calendar extends Component {
     }
 
     render() {
+
+        const firstDay = new Date(this.state.selectedDate.getFullYear(), this.state.selectedDate.getMonth()).getDay();
+        const month = this.state.selectedDate.getMonth();
+        const daysArr = Array.apply(null, {length: this.daysInMonth[month] + 1}).map(Number.call, Number);
+        daysArr.shift();
+
+        if ( firstDay > 1 ) {
+            const previousMonth = month === 0 ? 11 : month-1;
+            console.log(previousMonth)
+            const previousMonthDays = this.daysInMonth[previousMonth];
+            const previousMonthDaysArr = Array.apply(null, {length: previousMonthDays + 1}).map(Number.call, Number);
+            previousMonthDaysArr.shift();
+            for (let i = 0; i < firstDay - 1; i++) {
+                daysArr.unshift(previousMonthDaysArr[previousMonthDaysArr.length -1 -i])
+            }
+        }
+
+        const daysLeftInLastWeek = daysArr.length % 7;
+        for (let i = 1; i < daysLeftInLastWeek + 7 ; i++) {
+            daysArr.push(i);
+        }
+
+        const daysInWeek = 7;
+        const totalDaysDisplayed = 42;
+        const daysInWeeksArr = []
+        for (let i = 0; i < totalDaysDisplayed; i = i + daysInWeek ) {
+            daysInWeeksArr.push(daysArr.slice(i, i+daysInWeek));
+        }
+
+        console.log(daysInWeeksArr);
+
+
         return (
             <div className="calendarContainer">
                 <div className="calendarNavbar">
                     <div>{this.monthLabels[this.state.selectedDate.getMonth()]} {this.state.selectedDate.getFullYear()}</div>
                     <div className="calendarArrows">
-                        <span onClick={this.testFunc}>&lt;</span> <span>&gt;</span>
+                        <span onClick={this.calendarLeft}>&lt;</span> <span onClick={this.calendarRight}>&gt;</span>
                     </div>
                 </div>
                 <div className="calendarBlock">
@@ -38,6 +79,21 @@ export class Calendar extends Component {
                         {
                             this.dayLetters.map((letter, index) => 
                                 <div key={index}>{letter}</div>
+                            )
+                        }
+                    </div>
+                    <div className="daysOfMonth">
+                        {
+                            daysInWeeksArr.map((week, index) => 
+                                {
+                                    return (
+                                        <div key={index} className="weekRow">
+                                            {week.map((date, index) => 
+                                                <div key={index} className="dayInRow">{date}</div>
+                                            )}
+                                        </div>
+                                    )
+                                }
                             )
                         }
                     </div>
